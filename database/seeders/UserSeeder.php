a4c08f6b386435c11c92d50e9e0d049c6bf81293
Muhammad Saad <msaad@vfairs.com>
2022-10-27 04:25:39 +0500

    updates

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,9 +15,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::query()->upsert([
-            ['name' => 'bilal', "email" => "bilal@gmail.com", "username" => "bilal", "password" => Hash::make('Bilal@123'),"is_admin" => 1 ,"active" => "1"],
-            ['name' => 'hassan', "email" => "hassansaleem@gmail.com", "username" => "hassan", "password" => Hash::make('Hassan@123'),"is_admin" => 1 ,"active" => "1"]
-        ],['email', 'username'],['email', 'username']);
+        
     }
 }
